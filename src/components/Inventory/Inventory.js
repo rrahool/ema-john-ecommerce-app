5d7520de47e29d7cec43be5c163ef4b18ae7f6fd
rrahool <rbiswas596@gmail.com>
2020-04-06 21:43:18 +0600

    inventory data inserted to mongo cloud db

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import fakeData from '../../fakeData';
 
 const Inventory = () => {
+
+    const handleAddInventory = () => {
+        const product = fakeData[0];
+        console.log("Before post", fakeData.length);
+        fetch('http://localhost:4000/addProduct', {
+            method: 'POST', 
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(fakeData) 
+          }).then(res => res.json())
+          .then(data => {
+            console.log("Post Success", data);
+        });
+  
+    }
+
     return (
         <div>
             <h1>Inventory coming soon...</h1>
+            <button onClick={handleAddInventory} >Add Inventory</button>
         </div>
     );
 };
